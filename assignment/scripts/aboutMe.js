e012aa597a4ee472798ad9c0b4d7853262b32cd8
Desiree Quade <nekodez@gmail.com>
2021-11-04 22:09:33 -0500

    finished question 18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -51,12 +51,14 @@ else{
 
 console.log('testing: change adventurous to false');
 adventurous=false;
+
 if (adventurous){
   console.log('Adventures are great!');
 }
 else{
   console.log('How about we stay home?');
 }
+
 console.log('both options work but I prefer adventurous');
 adventurous=true;
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
@@ -68,9 +70,11 @@ if (luckyNumber==2&&adventurous){
 
 console.log('testing: change lucky number to 2');
 luckyNumber=2;
+
 if (luckyNumber==2&&adventurous){
   console.log('Role the dice!');
 }
+
 console.log('condition works but I prefer lucky number 17');
 luckyNumber=17;
 // 15 - Write a conditional that console logs "I can have more pets!"
@@ -92,8 +96,10 @@ else{
 console.log('testing: I had to give one pet to a friend');
 pets--;
 friendsPets++;
+
 console.log('number of pets:',pets);
 console.log('number of friendsPets:',friendsPets);
+
 if (pets<allowedPets){
   console.log('I can have more pets!');
 }
@@ -103,8 +109,10 @@ else if (pets==allowedPets){
 else{
   console.log('Oh no, I have too many pets!');
 }
+
 console.log('testing: I got 3 more pets!');
 pets+=3;
+
 if (pets<allowedPets){
   console.log('I can have more pets!');
 }
@@ -114,10 +122,13 @@ else if (pets==allowedPets){
 else{
   console.log('Oh no, I have too many pets!');
 }
+
 console.log('How many pets do I have:',pets);
 console.log('I only need my pup neko! I give all other pets to friends');
+
 pets-=4;
 friendsPets+=4;
+
 console.log('number of pets:',pets);
 console.log('number of friends pets:',friendsPets);
 // STRETCH GOALS:
@@ -138,10 +149,11 @@ else if (friendsPets>pets){
 
 console.log(mostPets);
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
-console.log('testing: i have more pets than friends pets');
+console.log('testing: I have more pets than friends pets');
 
 switch (pets>friendsPets) {
   case true:
+// I put in console.log so I can track which one runs
   console.log('true');
   mostPets=pets;
   break;
@@ -153,3 +165,5 @@ switch (pets>friendsPets) {
 
 console.log(mostPets);
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+adventurous=true? console.log('adventures are great!'):
+console.log('How about we stay home?');
